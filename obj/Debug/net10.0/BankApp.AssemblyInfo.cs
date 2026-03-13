72848a8fa62c36e79cfb7232ac6a86d4387eca2e
michaelsedaya <sedayamichael82@gmail.com>
2026-03-13 13:55:30 +0800

    Update

--- obj/Debug/net10.0/BankApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/BankApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f5a6f1f47534d98ffd07949a748a12b0df08271")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
